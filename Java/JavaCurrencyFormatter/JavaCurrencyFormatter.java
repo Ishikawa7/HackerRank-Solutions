546d5220fc4dd246a4ec2be1ad45e735fd969119
Ishikawa7 <davidegrimaldi92@gmail.com>
2020-05-07 23:09:09 +0200

    Added some solutions

--- Java/JavaCurrencyFormatter/JavaCurrencyFormatter.java
+++ Java/JavaCurrencyFormatter/JavaCurrencyFormatter.java
@@ -3,25 +3,24 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-/**
- * JavaCurrencyFormatter
- */
-public class JavaCurrencyFormatter {
 
+public class Solution {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
         scanner.close();
 
-        NumberFormat nF = NumberFormat.getCurrencyInstance();
-  
-        nF.setCurrency(Currency.getInstance(Locale.US)); 
-  
-        //String us = nF.getCurrency().getDisplayName(); 
+        Locale indiaLocale = new Locale("en", "IN");
+
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
         
-        //System.out.println("US: " + us);
-        //System.out.println("India: " + india);
-        //System.out.println("China: " + china);
-        //System.out.println("France: " + france);
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
     }
 }
